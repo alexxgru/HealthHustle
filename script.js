bed100aadafe1cc390649ebb762914eb8da600f3
alexxgru <alexgrundell@hotmail.com>
2023-03-10 13:07:10 +0100

    Vue fix

--- script.js
+++ script.js
@@ -7,27 +7,57 @@ class Exercise{
     }
 }
 
-Vue.CreateApp( {
+class Workout {
+    constructor(name,exercises) {
+        this.name = name,
+        this.Wexercises = exercises;
+    }
+}
+
+Vue.createApp( {
     data() {
         return {
-            exercises: []  
+            exercises: [
+            ],
+            personalExcersises: [
+            ],
+            workouts: [
+            ] 
         } 
         
     },
     
-    async created() {
-        try{
-            const response = await fetch(exercises.json);
-            const data = await response.json();
-            this.exercises = data.map(exercise => new Exercise(exercise.name, exercise.description, exercise.muscleGroups));
-        }
-        catch(error){
-            console.error('Error fetching exercises:', error)
-        }
-
-    },
-
     methods: {
-
+        createWorkouts(){
+            let exercisesToAdd = [];
+            for (let i = 0; i < 4; i++) {
+                exercisesToAdd.push(this.exercises[i])
+            }
+            let workout =  new Workout("Workout 1", exercisesToAdd);
+            this.workouts.push(workout);
+        }, 
+        async created() {
+            return new Promise(async resolve => {
+                try{
+                    const response = await fetch("exercises.json");
+                    const data = await response.json();
+        
+                    for (let i = 0; i < data.exercises.length; i++) {
+                        let ex = new Exercise (data.exercises[i].name, data.exercises[i].description, data.exercises[i].muscleGroups)
+                        this.exercises.push(ex);
+                    }
+                    resolve();
+                }
+                catch(error){
+                    console.error('Error fetching exercises:', error)
+                    resolve();
+                }
+            })
+    
+        }
     },
+    async beforeMount() {
+        await this.created();
+        this.createWorkouts();
+    }
 }).mount("#app")
