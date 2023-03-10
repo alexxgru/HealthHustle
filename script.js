8e8f2398ee6e2668699cc554a12d757ab1374ae8
rickardthunborg <rickard.a.thunborg@gmail.com>
2023-03-10 12:16:22 +0100

    better json

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
+class Exercise{
+    constructor(name, description, muscleGroups){
+        this.name = name;
+        this.description = description;
+        this.muscleGroups = muscleGroups;
+
+    }
+}
+
 Vue.CreateApp( {
     data() {
         return {
-            workouts: []  
+            exercises: []  
         } 
         
     },
+    
+    async created() {
+        try{
+            const response = await fetch(exercises.json);
+            const data = await response.json();
+            this.exercises = data.map(exercise => new Exercise(exercise.name, exercise.description, exercise.muscleGroups));
+        }
+        catch(error){
+            console.error('Error fetching exercises:', error)
+        }
+
+    },
+    
     methods: {
 
     },
 
-    class: {
-
-    }
 })
