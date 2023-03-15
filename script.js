e0018775f375a6b7e9cfb96ba4119eeaac406f94
alexxgru <alexgrundell@hotmail.com>
2023-03-15 11:11:43 +0100

    add workouts

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-class Exercise{
-    constructor(name, description, muscleGroups){
+class Exercise {
+    constructor(name, description, muscleGroups) {
         this.name = name;
         this.description = description;
         this.muscleGroups = muscleGroups;
@@ -8,56 +8,80 @@ class Exercise{
 }
 
 class Workout {
-    constructor(name,exercises) {
+    constructor(name, exercises) {
         this.name = name,
-        this.Wexercises = exercises;
+            this.Wexercises = exercises;
     }
 }
 
-Vue.createApp( {
+Vue.createApp({
     data() {
         return {
+            //Variables for creating a workout
+            popup: false,
+            exercisesInWorkout: [],
+            workoutname: "",
+
+
+
             selectedWorkout: new Workout("", []),
             exercises: [
             ],
             personalExcersises: [
             ],
             workouts: [
-            ] 
-        } 
-        
+            ]
+        }
+
     },
-    
+
     methods: {
-        createWorkouts(){
+        createWorkouts() {
             let exercisesToAdd = [];
             for (let i = 0; i < 4; i++) {
                 exercisesToAdd.push(this.exercises[i])
             }
-            let workout =  new Workout("Workout 1", exercisesToAdd);
+            let workout = new Workout("Workout 1", exercisesToAdd);
             let workout2 = new Workout("Workout 2", [this.exercises[1]]);
             this.workouts.push(workout);
             this.workouts.push(workout2);
             this.selectedWorkout = workout;
-        }, 
+        },
         async created() {
             return new Promise(async resolve => {
-                try{
+                try {
                     const response = await fetch("exercises.json");
                     const data = await response.json();
-        
+
                     for (let i = 0; i < data.exercises.length; i++) {
-                        let ex = new Exercise (data.exercises[i].name, data.exercises[i].description, data.exercises[i].muscleGroups)
+                        let ex = new Exercise(data.exercises[i].name, data.exercises[i].description, data.exercises[i].muscleGroups)
                         this.exercises.push(ex);
                     }
                     resolve();
                 }
-                catch(error){
+                catch (error) {
                     console.error('Error fetching exercises:', error)
                     resolve();
                 }
             })
-    
+        },
+        closePopup() {
+            this.popup = false;
+            this.exercisesInWorkout = [];
+            this.workoutname = "";
+        },
+        addExerciseToWorkout(ex) {
+            const index = this.exercisesInWorkout.findIndex(x => x.name === ex.name);
+            if (index !== -1) {
+                this.exercisesInWorkout.splice(index, 1);
+            } else {
+                this.exercisesInWorkout.push(ex);
+            }
+        },
+        addWorkout() {
+            let workout = new Workout(this.workoutname, this.exercisesInWorkout);
+            this.workouts.push(workout);
+            this.closePopup();
         }
     },
     async beforeMount() {
